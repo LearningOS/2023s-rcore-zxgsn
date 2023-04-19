23278cd1019d3ca49b86967539f272f352a01628
bibo <97893760+zxgsn@users.noreply.github.com>
2023-04-19 21:30:36 +0800

    文档未完成，先测试一下ci

---
+++ src/syscall/process.rs
@@ -0,0 +1,77 @@
+//! Process management syscalls
+// use core::char::MAX;
+
+use crate::{
+    config::MAX_SYSCALL_NUM,
+    task::{exit_current_and_run_next, suspend_current_and_run_next, TaskStatus, get_current_task_state, get_init_time, get_tcb_syscall_times},
+    timer::{get_time_us, get_time_ms},
+};
+
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct TimeVal {
+    pub sec: usize,
+    pub usec: usize,
+}
+
+/// Task information
+#[allow(dead_code)]
+pub struct TaskInfo {
+    /// Task status in it's life cycle
+    status: TaskStatus,
+    /// The numbers of syscall called by task
+    syscall_times: [u32; MAX_SYSCALL_NUM],
+    /// Total running time of task
+    time: usize,
+}
+/*
+impl TaskInfo {
+    pub fn set_syscall_times(&mut self, new_syscall_times: [u32; MAX_SYSCALL_NUM]){
+        self.syscall_times = new_syscall_times;
+    }
+}
+*/
+
+/// task exits and submit an exit code
+pub fn sys_exit(exit_code: i32) -> ! {
+    trace!("[kernel] Application exited with code {}", exit_code);
+    exit_current_and_run_next();
+    panic!("Unreachable in sys_exit!");
+}
+
+/// current task gives up resources for other tasks
+pub fn sys_yield() -> isize {
+    trace!("kernel: sys_yield");
+    suspend_current_and_run_next();
+    0
+}
+
+/// get time with second and microsecond
+pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
+    trace!("kernel: sys_get_time");
+    let us = get_time_us();
+    unsafe {
+        *ts = TimeVal {
+            sec: us / 1_000_000,
+            usec: us % 1_000_000,
+        };
+    }
+    0
+}
+
+/// YOUR JOB: Finish sys_task_info to pass testcases
+pub fn sys_task_info(_ti: *mut TaskInfo) -> isize {
+    trace!("kernel: sys_task_info");
+    let status = get_current_task_state();
+    let time = get_time_ms() - get_init_time();
+    let syscall_times = get_tcb_syscall_times();
+    unsafe {
+        *_ti = TaskInfo {
+            status,
+            syscall_times,
+            time,
+        }
+    }
+    0
+}
